68547aa20dfc63288660617856b7babcb4a35753
vioif <ireneaflores3@gmail.com>
2018-01-22 14:52:19 -0500

    created users and password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109195556) do
+ActiveRecord::Schema.define(version: 20180119193908) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "artist"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20180109195556) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
